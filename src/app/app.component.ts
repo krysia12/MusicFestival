8eeb5ef48522e6eb390da6380bad80f305d84f8f
Sequeira <krysia.sequeira@capgemini.com>
2021-10-19 10:31:34 +1100

    Added caching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FestivalService } from './festival.service';
 
 @Component({
@@ -6,9 +6,10 @@ import { FestivalService } from './festival.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   title = 'Music Festival';
-  festivals:any[] = [];
+  festivals: any[] = [];
+  recordLabelDets: any[] = [];
   constructor(private festivalservice: FestivalService) { }
 
   ngOnInit() {
--- src/app/festival.service.ts
+++ src/app/festival.service.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FestivalService {
 
-  constructor(private httpClient: HttpClient) {}
+  festivals: Observable<any>;
+
+  constructor(private httpClient: HttpClient) { }
 
   getFestivals() {
-    let headers = new HttpHeaders();
-    headers.append('Content-Type','application/json');
-      return this.httpClient.get('/api/v1/festivals',{
-        headers: headers
-      });
+    if(!this.festivals){
+       let headers = new HttpHeaders();
+           headers.append('Content-Type','application/json');
+             return this.httpClient.get('/api/v1/festivals',{
+               headers: headers
+             }).pipe(
+                    shareReplay(1)
+                 );
+    }
+    return this.festivals;
+
     }
  }
