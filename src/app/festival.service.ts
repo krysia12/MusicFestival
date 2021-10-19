e6740b2cb0173f4dfd117b2679910f37aa18e099
Sequeira <krysia.sequeira@capgemini.com>
2021-10-19 11:27:25 +1100

    Error handling

--- src/app/festival.service.ts
+++ src/app/festival.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { map, shareReplay, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -19,10 +19,37 @@ export class FestivalService {
              return this.httpClient.get('/api/v1/festivals',{
                headers: headers
              }).pipe(
-                    shareReplay(1)
-                 );
+                    shareReplay(1),
+                    catchError(error => {
+                                            let errorMsg: string;
+                                            if (error.error instanceof ErrorEvent) {
+                                                errorMsg = `Error: ${error.error.message}`;
+                                            } else {
+                                                errorMsg = this.getServerErrorMessage(error);
+                                            }
+                                            return throwError(errorMsg);
+                                        }
+                 ));
     }
     return this.festivals;
 
     }
+
+    private getServerErrorMessage(error: any): string {
+            switch (error.status) {
+                case 404: {
+                    return `Not Found: ${error.message}`;
+                }
+                case 403: {
+                    return `Access Denied: ${error.message}`;
+                }
+                case 500: {
+                    return `Internal Server Error: ${error.message}`;
+                }
+                default: {
+                    return `Unknown Server Error: ${error.message}`;
+                }
+
+            }
+        }
  }
